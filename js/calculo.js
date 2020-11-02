2d525313bfafc6d1d677c245964b7b81beb89ed4
Bruno Tadeu <brunthadeu@gmail.com>
2020-11-01 22:11:35 -0300

    tratamento de strings para numeros da function calculo

--- js/calculo.js
+++ js/calculo.js
@@ -16,7 +16,58 @@ function clica(botao) {
     }
 }
 
-function calculo(i) {
-    let res = eval(i)
-    teste.innerText = res
+function calculo(x) {
+    let arr = [] = x.split(""); //transforma a string valor em um array
+
+    for(let i = 0; i < x.length; i++) {
+
+        if(x[i] === "-" || x[i] === "+" || x[i] === "*" || x[i] === "/") {
+
+            var posicaoOperador = x.indexOf(x[i]); //guarda a posicao do operador aritmetico
+            var operador = x[i] //guarda o operador aritmetico
+        }
+    }
+
+    let guardaArrNum1 = arr.splice(0, posicaoOperador); //guarda os numeros do array que vem antes do operador aritmetico
+
+    let num1 = guardaArrNum1.join(""); //concatena os numeros do array que vem antes do operador aritmetico
+
+    let guardaArrNum2 = arr.splice(0, 1); //remove o operador aritmetico
+
+    let num2 = arr.join(""); //concatena os numeros do array que vem depois do operador aritmetico
+
+    var result = 0;
+    
+    //faz trata as strings e faz o calculo
+    switch (operador) {
+
+        case "-": {
+            result = parseFloat(num1) - parseFloat(num2);
+            break;
+        }
+
+        case "+": {
+            result = parseFloat(num1) + parseFloat(num2);
+            break;
+        }
+
+        case "*": {
+            result = parseFloat(num1) * parseFloat(num2);
+            break;
+        }
+
+        case "/": {
+            result = parseFloat(num1) / parseFloat(num2);
+            break;
+        }
+    }
+
+    teste.innerText = result
 }
+
+
+//função original (escrita pela Scarlate)
+//function calculo2(i) {
+//   let res = eval(i)
+//    teste.innerText = res
+//}
