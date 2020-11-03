276476792eb1b8b78ce8b6d4f919b3230c7d7a7c
LaDespistada1981 <roberta.silva@al.resilia.com.br>
2020-11-03 11:51:23 -0300

    ajuste de layout - html e css

---
+++ js/calculo.js
@@ -0,0 +1,73 @@
+var display = document.getElementById("display")
+var valor = ""
+display.textContent = valor
+
+//verificando e concatenando string
+function clica(botao) {
+    valor = display.textContent += botao
+        // if (botao == "E") {
+        //     apagar(valor)
+        // }
+
+}
+
+//limpar tela
+function reset() {
+    display.textContent = ""
+}
+
+//apaga um elemento de cada vez
+// function apagar(valor) {
+//     var res = valor.split('')
+//     res.pop()
+//     res.pop()
+//     var string = res.join('')
+//     return display.textContent = string
+
+// }
+
+function calculo(x) {
+
+
+
+    let arr = [] = x.split(""); //transforma a string valor em um array
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] === "-" || x[i] === "+" || x[i] === "*" || x[i] === "/") {
+            var posicaoOperador = x.indexOf(x[i]); //guarda a posicao do operador aritmetico
+            var operador = x[i] //guarda o operador aritmetico
+        }
+    }
+    let guardaArrNum1 = arr.splice(0, posicaoOperador); //guarda os numeros do array que vem antes do operador aritmetico
+    let num1 = guardaArrNum1.join(""); //concatena os numeros do array que vem antes do operador aritmetico
+    let guardaArrNum2 = arr.splice(0, 1); //remove o operador aritmetico
+    let num2 = arr.join(""); //concatena os numeros do array que vem depois do operador aritmetico
+    var result = 0;
+    //faz trata as strings e faz o calculo
+    switch (operador) {
+        case "-":
+            {
+                result = parseFloat(num1) - parseFloat(num2);
+                break;
+            }
+        case "+":
+            {
+                result = parseFloat(num1) + parseFloat(num2);
+                break;
+            }
+        case "*":
+            {
+                result = parseFloat(num1) * parseFloat(num2);
+                break;
+            }
+        case "/":
+            {
+                result = parseFloat(num1) / parseFloat(num2);
+                break;
+            }
+    }
+    //resltado fora do padrao
+    if (result > 0 || result < 0)
+        return display.textContent = result
+    return display.textContent = x
+
+}
